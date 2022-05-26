c1a83a2532d8347305e412547dba295874982529
Nabin Soni <nabinkumar570@gmail.com>
2022-05-27 00:06:16 +0530

    Initial Commit

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None // This will enable css styles to all project components
 })
 export class ServerElementComponent implements OnInit {
  @Input('srvElement') element: {type: string, name: string, content: string};
@@ -11,5 +12,7 @@ export class ServerElementComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+onSubmit(name){
+  console.log(name.value);
+}
 }
